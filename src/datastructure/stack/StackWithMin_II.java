a576582765e99ecfa43d30d10b0c1eebc7f31ecc
Julia Chen Jing <juliachenjing@gmail.com>
2017-06-04 20:33:35 -0500

    Update StackWithMin_II.java

--- src/datastructure/stack/StackWithMin_II.java
+++ src/datastructure/stack/StackWithMin_II.java
@@ -4,12 +4,12 @@ import java.util.Stack;
 
 /*
  *  How would you design a stack which, in addition to push and pop, also has a function
-min which returns the minimum element? Push, pop and min should all operate in
-O(1)
+    min which returns the minimum element? Push, pop and min should all operate in
+    O(1)
  */
 public class StackWithMin_II extends Stack<Integer> {
 	/*
-	 * There’s just one issue with this: if we have a large stack, we waste a
+	 * Thereâ€™s just one issue with solution I:if we have a large stack, we waste a
 	 * lot of space by keeping track of the min for every single element. Can we
 	 * do better? 
 	 * 
@@ -17,8 +17,8 @@ public class StackWithMin_II extends Stack<Integer> {
 	 * additional stack which keeps track of the mins
 	 * 
 	 * Why might this be more space efficient? If many elements have the same
-	 * local min, then we’re keeping a lot of duplicate data. By having the mins
-	 * kept in a separate stack, we don’t have this duplicate data (although we
+	 * local min, then weâ€™re keeping a lot of duplicate data. By having the mins
+	 * kept in a separate stack, we donâ€™t have this duplicate data (although we
 	 * do use up a lot of extra space because we have a stack node instead of a
 	 * single int).
 	 */
@@ -34,7 +34,7 @@ public class StackWithMin_II extends Stack<Integer> {
 		if (value <= min()) {
 			s2.push(value);
 		}
-		super.push(value);
+		super.push(value);//this class is originally a stack
 	}
 
 	public Integer pop() {
