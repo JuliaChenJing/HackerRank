012f8a50c167df812abfd0dc5681ff0104eb3323
Julia Chen <juliachenjing@gmail.com>
2017-06-07 20:20:09 -0500

    2

---
+++ src/datastructure/graph/RouteBetweenNodes.java
@@ -0,0 +1,70 @@
+package datastructure.graph;
+
+import java.util.LinkedList;
+
+/*
+ *  Given a directed graph, design an algorithm to find out whether 
+ *  there is a route between two nodes.
+ */
+public class RouteBetweenNodes {
+
+	public enum State {
+		Unvisited, Visited, Visiting;
+	}
+
+	/*
+	 * 
+	 * This problem can be solved by just simple graph traversal, such as depth
+	 * first search or breadth first search.   We start with one of the two nodes
+	 * and, during traversal, check if the other node is found. We should mark
+	 * any node found in the course of the algorithm as "already visited" to
+	 * avoid cycles and repetition of the nodes.
+	 */
+	
+	public static boolean search(Graph graph, Node from, Node to) {
+		
+		LinkedList<Node> relinkedlist = new LinkedList<Node>(); // operates as Stack
+		//set all nodes in graph Unvisited
+		for (Node u : graph.getNodes()) {
+			u.state = State.Unvisited;
+		}
+		
+		from.state = State.Visiting;
+		relinkedlist.add(from);//add the first node to the relinkedlist
+		Node u;
+		while (!relinkedlist.isEmpty()) {
+			u = relinkedlist.removeFirst(); // i.e., pop()
+			if (u != null) {
+				for (Node v : u.getAdjacent()) {
+					if (v.state == State.Unvisited) {
+						if (v == to) {
+							return true;
+						} else {
+							v.state = State.Visiting;
+							relinkedlist.add(v);
+						}
+					}
+				}
+				u.state = State.Visited;
+			}
+		}
+		return false;
+	}
+
+	class Graph {
+		Node[] nodes;
+
+		Node[] getNodes() {
+			return nodes;
+		}
+	}
+
+	class Node {
+		State state;
+
+		public Node[] getAdjacent() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	}
+}
---
+++ src/datastructure/tree/InOrderSuccessor.java
@@ -0,0 +1,67 @@
+package datastructure.tree;
+
+/*
+ *     Write an algorithm to find the ‘next’ node (e.g., in-order successor)
+ *     of a given node in a binary search tree where each node has a link
+ *     to its parent.
+ */
+public class InOrderSuccessor {
+
+	class TreeNode {
+		TreeNode left;
+		TreeNode right;
+		TreeNode parent;//each node has a link  to its parent
+		int value;
+
+		TreeNode(int v) {
+			value = v;
+		}
+	}
+
+	/*
+	 * Solution: 
+	 *  On an in-order traversal, we visit
+	 * X.left, then X, then X.right. So, if we want to find X.successor(), we do
+	 * the following:
+	 * 
+	 * 1. If X has a right child, then the successor must be on the right side
+	 * of X (because of the order in which we visit nodes). Specifically, the
+	 * left-most child must be the first node visited in that subtree. 2. Else,
+	 * we go to X 's parent (call it P).
+	 * 
+	 * 2.a. If X was a left child (P.left = X), then P is the successor of X 
+	 * 2.b. If X was a right child (P.right = X), then we have fully visited P, 
+	 * so we call successor(P).
+	 */
+	
+	public static TreeNode inorderSucc(TreeNode e) {
+		if (e == null)
+			return null;
+
+		TreeNode p;
+		// Found right children ‐> return 1st inorder node on right
+		if (e.parent == null || e.right != null) {
+			p = leftMostChild(e.right);
+		} else {
+
+			// Go up until we’re on left instead of right (case 2b)
+			while ((p = e.parent) != null) {
+				if (p.left == e) {
+					break;
+				}
+				e = p;
+			}
+		}
+		return p;
+
+	}
+
+	// find the left most child of the tree which has the parent node as e
+	public static TreeNode leftMostChild(TreeNode e) {
+		if (e == null)
+			return null;
+		while (e.left != null)
+			e = e.left;
+		return e;
+	}
+}
