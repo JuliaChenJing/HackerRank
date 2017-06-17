86c4e941d9ef37899b08584747f0088ea18decd6
Julia Chen Jing <juliachenjing@gmail.com>
2017-06-17 16:25:58 -0500

    Update FindNthToLast.java

--- src/datastructure/linkedlist/FindNthToLast.java
+++ src/datastructure/linkedlist/FindNthToLast.java
@@ -104,6 +104,38 @@ public class FindNthToLast {
 
 		System.out.println(re_2.data);
 	}
+	// HackerRank: hackerrank.com/rshaghoulian
+
+/*
+  Get Nth element from the end in a linked list of integers
+  Number of elements in the list will always be greater than N.
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+
+//  Time Complexity: O(n)
+// Space Complexity: O(1)
+int GetNode(Node head, int k) {
+    Node curr   = head;
+    Node runner = head;
+    
+    /* Move runner into the list by k elements */
+    for (int i = 0; i < k; i++) {
+        runner = runner.next;
+    }
+    
+    /* Move both pointers */
+    while (runner.next != null) {
+        curr   = curr.next;
+        runner = runner.next;
+    }
+    
+    return curr.data;
+}
+	
 }
 
 
