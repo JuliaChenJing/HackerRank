8c6aa24b787346fd98115fe7e3d76973272a551d
Julia Chen Jing <juliachenjing@gmail.com>
2017-06-17 22:41:14 -0500

    Update Traversal.java

--- src/datastructure/tree/Traversal.java
+++ src/datastructure/tree/Traversal.java
@@ -29,3 +29,27 @@ void inOrder(Node root) {
       inOrder(root.right);
   }
 }
+
+
+//https://www.hackerrank.com/challenges/tree-top-view/problem
+void topView(Node root) {
+    if (root != null) {
+        printLeft(root.left);
+        System.out.print(root.data + " ");
+        printRight(root.right);
+    }
+}
+
+void printLeft(Node n) {
+    if (n != null) {
+        printLeft(n.left);
+        System.out.print(n.data + " ");
+    }
+}
+
+void printRight(Node n) {
+    if (n != null) {
+        System.out.print(n.data + " ");
+        printRight(n.right);
+    }
+}
