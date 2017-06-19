02244d5e3bbace6f07d92d9768a6f36d0c62372f
Julia Chen <juliachenjing@gmail.com>
2017-06-18 22:35:02 -0500

    sort

---
+++ src/algorithm/sorting/BucketSort.java
@@ -0,0 +1,28 @@
+package algorithm.sorting;
+
+public class BucketSort {
+	/*
+	 * Understanding the common sorting algorithms is incredibly valuable, as
+	 * many sorting or searching solutions require tweaks of known sorting
+	 * algorithms. A good approach when you are given a question like this is to
+	 * run through the different sorting algorithms and see if one applies
+	 * particularly well.
+	 * 
+	 * 
+	 * Example: You have a very large array of ‘Person’ objects. Sort the people
+	 * in increasing order of age. We’re given two interesting bits of knowledge
+	 * here: (1) It’s a large array, so efficiency is very important. (2) We are
+	 * sorting based on ages, so we know the values are in a small range. By
+	 * scanning through the various sorting algorithms, we might notice that
+	 * bucket sort would be a perfect candidate for this algorithm. In fact, we
+	 * can make the buckets small (just 1 year each) and get O(n) running time.
+	 * 
+	 * 
+	 */
+	/*
+	 * Partition the array into a finite number of buckets, and then sort each
+	 * bucket individually. This gives a time of O(n + m), where n is the number
+	 * of items and m is the number of distinct items.
+	 */
+
+}
