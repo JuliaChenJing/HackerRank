337cc400984b8ab9468911702c5a58139a1b1b4f
Julia Chen <juliachenjing@gmail.com>
2017-06-21 23:30:31 -0500

    test

---
+++ src/datastructure/array/Island.java
@@ -0,0 +1,35 @@
+package datastructure.array;
+
+public class Island {
+
+	static int getNumberOfIslands(int[][] binaryMatrix) {
+
+		int re = 0;
+		int sizeX = binaryMatrix.length;
+		int sizeY = binaryMatrix[0].length;
+
+		for (int i = 0; i < sizeX; i++)
+			for (int j = 0; j < sizeY; j++) {
+				if (binaryMatrix[i][j] == 1) 
+				{
+				
+					//left top corner
+					if(i==0 &&j==0 &&binaryMatrix[i ][j]==1)
+						re++;
+					
+					if ((i - 1 >= 0 && binaryMatrix[i - 1][j] != 1)
+							|| (j - 1 >= 0 && binaryMatrix[i][j - 1] != 1))
+						re++;
+				}
+			}
+
+		return re;
+
+	}
+
+	public static void main(String[] args) {
+		int[][] binaryMatrix = { { 1, 1 }, { 0, 1 } ,{ 0, 1 } };
+		System.out.println(getNumberOfIslands(binaryMatrix));
+	}
+
+}
---
+++ src/datastructure/hashtable/FlattenDictionary.java
@@ -0,0 +1,75 @@
+package datastructure.hashtable;
+import java.util.HashMap;
+
+public class FlattenDictionary {
+
+	/*
+input:  dict = {
+            "Key1" : "1",
+            "Key2" : {
+                "a" : "2",
+                "b" : "3",
+                "c" : {
+                    "d" : "3",
+                    "e" : "1"
+                }
+            }
+        }
+
+output: {
+            "Key1" : "1",
+            "Key2.a" : "2",
+            "Key2.b" : "3",
+            "Key2.c.d" : "3",
+            "Key2.c.e" : "1"
+        }
+	 */
+
+	static HashMap<String, String> flattenDictionary(HashMap<String, Object> dict) {
+		HashMap<String, String> flatDictionary = new HashMap<String, String>();
+
+		flattenDictionaryHelper("", dict, flatDictionary);
+
+		return flatDictionary;
+
+	}
+
+	static HashMap<String, String> flattenDictionaryHelper(String initialKey, HashMap<String, Object> dict,
+			HashMap<String, String> flatDictionary) {
+		Object value;
+		for (String key : dict.keySet()) {
+
+			value = dict.get(key);
+
+			if (!isDictionary(value)) // test if the value is of a String of integer
+			{
+				if (initialKey == null || initialKey == "")
+					flatDictionary.put(key, (String) value);
+				else
+					flatDictionary.put(initialKey + "." + key, (String) value);
+			} else {
+
+				if (initialKey == null || initialKey == "")
+
+					flattenDictionaryHelper(key, (HashMap <String, Object>) value, flatDictionary);
+				else
+
+					flattenDictionaryHelper(initialKey + "." + key, (HashMap<String, Object>) value, flatDictionary);
+			}
+		}
+
+		return flatDictionary;
+	}
+
+	//
+
+	// test if the value is of a String of integer
+	private static boolean isDictionary(Object value) {
+		
+	   String s=(String)value;
+	   if (s.matches("/d"))
+		return true;
+	   return false;
+	}
+
+}
