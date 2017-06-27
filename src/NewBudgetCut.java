fbc1c1d54dfcd6d596b0b66c7f066718cb410ed9
Julia Chen <juliachenjing@gmail.com>
2017-06-26 21:43:29 -0500

    Update NewBudgetCut.java

--- src/NewBudgetCut.java
+++ src/NewBudgetCut.java
@@ -3,14 +3,14 @@ public class NewBudgetCut {
 
 	/*
 	 * The awards committee of your alma mater (i.e. your college/university)
-	 * asked for your assistance with a budget allocation problem they’re
+	 * asked for your assistance with a budget allocation problem theyâ€™re
 	 * facing. Originally, the committee planned to give N research grants this
 	 * year. However, due to spending cutbacks, the budget was reduced to
 	 * newBudget dollars and now they need to reallocate the grants. The
-	 * committee made a decision that they’d like to impact as few grant
+	 * committee made a decision that theyâ€™d like to impact as few grant
 	 * recipients as possible by applying a maximum cap on all grants. Every
 	 * grant initially planned to be higher than cap will now be exactly cap
-	 * dollars. Grants less or equal to cap, obviously, won’t be impacted.
+	 * dollars. Grants less or equal to cap, obviously, wonâ€™t be impacted.
 	 * 
 	 * Given an array grantsArray of the original grants and the reduced budget
 	 * newBudget, write a function findGrantsCap that finds in the most
@@ -66,7 +66,7 @@ public class NewBudgetCut {
 
 		int num = grantsArray.length;// store the length of array
 		for (int i = num - 1; i >= num - cutNum; i--)
-			if (grantsArray[i] > num - cutNum)
+			if (grantsArray[i] >cut)
 				return false;
 		return true;
 	}
