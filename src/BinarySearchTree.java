4198be42d7ba8fbf1e31eaf2a93dc053e78839f9
Julia Chen <juliachenjing@gmail.com>
2017-06-30 20:53:54 -0500

    tree

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,130 @@
+public class BinarySearchTree {
+
+	public static void main(String[] args) {
+
+		Node test = null, succ = null;
+
+		// Create a Binary Search Tree
+		BinarySearchTree tree = new BinarySearchTree();
+		tree.insert(20);
+		tree.insert(9);
+		tree.insert(25);
+		tree.insert(5);
+		tree.insert(12);
+		tree.insert(11);
+		tree.insert(14);
+
+		// Get a reference to the node whose key is 9
+		test = tree.getNodeByKey(14);
+
+		// Find the in order successor of test
+		succ = tree.findInOrderSuccessor(test);
+
+		// Print the key of the successor node
+		if (succ != null) {
+			System.out.println("Inorder successor of " + test.key + " is " + succ.key);
+		} else {
+			System.out.println("Inorder successor does not exist");
+		}
+	}
+
+	Node root;
+
+	Node findInOrderSuccessor(Node inputNode) {
+
+		if (inputNode.right != null) {// right node is not null
+
+			Node node = inputNode.right;
+			while (node.left != null)
+				node = node.left;
+			return node;
+		} else {// right node is null
+			Node parent = inputNode.parent;
+			Node node = inputNode;
+
+			while (parent.left != node) {
+				node = parent;
+				parent = parent.parent;
+			}
+
+			return parent;
+
+		}
+	}
+
+	// Given a binary search tree and a number, inserts a new node
+	// with the given number in the correct place in the tree
+	void insert(int key) {
+
+		// 1. If the tree is empty, create the root
+		if (root == null) {
+			root = new Node(key);
+			return;
+		}
+
+		// 2) Otherwise, create a node with the key
+		// and traverse down the tree to find where to
+		// to insert the new node
+		Node currentNode = root;
+		Node newNode = new Node(key);
+
+		while (currentNode != null) {
+			if (key < currentNode.key) {
+				if (currentNode.left == null) {
+					currentNode.left = newNode;
+					newNode.parent = currentNode;
+					break;
+				} else {
+					currentNode = currentNode.left;
+				}
+			} else {
+				if (currentNode.right == null) {
+					currentNode.right = newNode;
+					newNode.parent = currentNode;
+					break;
+				} else {
+					currentNode = currentNode.right;
+				}
+			}
+		}
+	}
+
+	// Return a reference to a node in the BST by its key.
+	// Use this method when you need a node to test your
+	// findInOrderSuccessor method on
+	Node getNodeByKey(int key) {
+		Node currentNode = root;
+
+		while (currentNode != null) {
+			if (key == currentNode.key) {
+				return currentNode;
+			}
+
+			if (key < currentNode.key) {
+				currentNode = currentNode.left;
+			} else {
+				currentNode = currentNode.right;
+			}
+		}
+
+		return null;
+	}
+
+	
+	class Node {
+
+		int key;
+		Node left;
+		Node right;
+		Node parent;
+
+		Node(int key) {
+			this.key = key;
+			left = null;
+			right = null;
+			parent = null;
+		}
+	}
+}
+
+
--- src/Path.java
+++ src/Path.java
@@ -1,163 +1,185 @@
 public class Path {
-	
-	//http://www.geeksforgeeks.org/count-possible-paths-top-left-bottom-right-nxm-matrix/
-	
-	//http://www.programcreek.com/2014/05/leetcode-unique-paths-java/
-	
-	//https://www.pramp.com/question/N5LYMbYzyOtbpovQoY7X
-	
-	/*8.2 p176
-	 * Imagine a robot sitting on the upper left hand corner of an NxN grid. The
-	 * robot can only move in two directions: right and down. How many possible
-	 * paths are there for the robot?
+
+	// http://www.geeksforgeeks.org/count-possible-paths-top-left-bottom-right-nxm-matrix/
+
+	// http://www.programcreek.com/2014/05/leetcode-unique-paths-java/
+
+	// https://www.pramp.com/question/N5LYMbYzyOtbpovQoY7X
+
+	/*
+	 * 8.2 p176 Imagine a robot sitting on the upper left hand corner of an NxN
+	 * grid. The robot can only move in two directions: right and down. How many
+	 * possible paths are there for the robot?
 	 * 
-	 * FOLLOW UP Imagine certain squares are limited, such that the robot
-	 * can not step on them. Design an algorithm to get all possible paths for
-	 * the robot.
+	 * FOLLOW UP Imagine certain squares are limited, such that the robot can
+	 * not step on them. Design an algorithm to get all possible paths for the
+	 * robot.
 	 */
 
 	// Returns count of possible paths to reach cell at row number m and column
 	// number n from the topmost leftmost cell (cell at 1, 1)
-	
-	
-	public static void main(String [] args)
-	{
-		System.out.println(numberOfPathsWithRecursion(4,4));
-		System.out.println(numberOfPathsWithRestriction(4,4));
-		System.out.println(uniquePathsWithDFS(4,4));
-		System.out.println(uniquePathsWithDFSWithRestriction(4,4));
-		System.out.println(uniquePathsWithDynamicProgramming(4,4));
-	
-		System.out.println(numberOfPaths_I(4,4));
+
+	public static void main(String[] args) {
+		System.out.println(numOfPathsToDest(4));
+		System.out.println(numberOfPathsWithRecursion(4, 4));
+		System.out.println(numberOfPathsWithRestriction(4, 4));
+		System.out.println(uniquePathsWithDFS(4, 4));
+		System.out.println(uniquePathsWithDFSWithRestriction(4, 4));
+		System.out.println(uniquePathsWithDynamicProgramming(4, 4));
+
+		System.out.println(numberOfPaths_I(4, 4));
 	}
-	
-	static int  numberOfPathsWithRecursion(int m, int n)
-	{
-	   // If either given row number is first or given column number is first
-	   if (m == 1 || n == 1)
-	        return 1;
-	 
-	   // If diagonal movements are allowed then the last addition
-	   // is required.
-	   return  numberOfPathsWithRecursion(m-1, n) + numberOfPathsWithRecursion(m, n-1);
-	           // + numberOfPaths(m-1,n-1);
+
+	static int destinationTimes = 0;
+
+	static int numOfPathsToDest(int n) {
+		findPaths(0, 0, n);
+
+		return destinationTimes;
 	}
-	
-	static int  numberOfPathsWithRestriction(int m, int n)
-	{
-	   // If either given row number is first or given column number is first
-	   if (m == 1 || n == 1)
-	        return 1;
-	   if(m<n)
-		   return 0;
-	 
-	   // If diagonal movements are allowed then the last addition
-	   // is required.
-	   return  numberOfPathsWithRestriction(m-1, n) + numberOfPathsWithRestriction(m, n-1);
-	           // + numberOfPaths(m-1,n-1);
+
+	static void findPaths(int i, int j, int n) {
+		if (i == (n - 1) && j == (n - 1)) {
+			destinationTimes++;
+		} else {
+			if (i >= (j + 1) && (j + 1) <= (n - 1)) {
+				findPaths(i, j + 1, n);
+			}
+
+			if ((i + 1) >= j && (i + 1) <= (n - 1)) {
+				findPaths(i + 1, j, n);
+			}
+		}
 	}
+
 	
-	//Returns count of possible paths to reach cell at row number m and column
+
+	static int numberOfPathsWithRecursion(int m, int n) {
+		// If either given row number is first or given column number is first
+		if (m == 1 || n == 1)
+			return 1;
+
+		// If diagonal movements are allowed then the last addition
+		// is required.
+		return numberOfPathsWithRecursion(m - 1, n) + numberOfPathsWithRecursion(m, n - 1);
+		// + numberOfPaths(m-1,n-1);
+	}
+
+	static int numberOfPathsWithRestriction(int m, int n) {
+		// If either given row number is first or given column number is first
+		if (m == 1 || n == 1)
+			return 1;
+		if (m < n)
+			return 0;
+
+		// If diagonal movements are allowed then the last addition
+		// is required.
+		return numberOfPathsWithRestriction(m - 1, n) + numberOfPathsWithRestriction(m, n - 1);
+		// + numberOfPaths(m-1,n-1);
+	}
+
+	// Returns count of possible paths to reach cell at row number m and column
 	// number n from the topmost leftmost cell (cell at 1, 1)
-	static int numberOfPaths_I(int m, int n)
-	{
-	    // Create a 2D table to store results of subproblems
-	    int [][]count=new int[m][n];
-	 
-	    // Count of paths to reach any cell in first column is 1
-	    for (int i = 0; i < m; i++)
-	        count[i][0] = 1;
-	 
-	    // Count of paths to reach any cell in first column is 1
-	    for (int j = 0; j < n; j++)
-	        count[0][j] = 1;
-	 
-	    // Calculate count of paths for other cells in bottom-up manner using
-	    // the recursive solution
-	    for (int i = 1; i < m; i++)
-	    {
-	        for (int j = 1; j < n; j++)
-	 
-	            // By uncommenting the last part the code calculatest he total
-	            // possible paths if the diagonal Movements are allowed
-	            count[i][j] = count[i-1][j] + count[i][j-1]; //+ count[i-1][j-1];
-	 
-	    }
-	    return count[m-1][n-1];
+	static int numberOfPaths_I(int m, int n) {
+		// Create a 2D table to store results of subproblems
+		int[][] count = new int[m][n];
+
+		// Count of paths to reach any cell in first column is 1
+		for (int i = 0; i < m; i++)
+			count[i][0] = 1;
+
+		// Count of paths to reach any cell in first column is 1
+		for (int j = 0; j < n; j++)
+			count[0][j] = 1;
+
+		// Calculate count of paths for other cells in bottom-up manner using
+		// the recursive solution
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++)
+
+				// By uncommenting the last part the code calculatest he total
+				// possible paths if the diagonal Movements are allowed
+				count[i][j] = count[i - 1][j] + count[i][j - 1]; // +
+																	// count[i-1][j-1];
+
+		}
+		return count[m - 1][n - 1];
 	}
-	
+
 	public static int uniquePathsWithDFS(int m, int n) {
-	    return dfs(0,0,m,n);
+		return dfs(0, 0, m, n);
 	}
-	 
-	public static int dfs(int i, int j, int m, int n){
-	    if(i==m-1 && j==n-1){
-	        return 1;
-	    }
-	 
-	    if(i<m-1 && j<n-1){
-	        return dfs(i+1,j,m,n) + dfs(i,j+1,m,n);
-	    }
-	 
-	    if(i<m-1){
-	        return dfs(i+1,j,m,n);
-	    }
-	 
-	    if(j<n-1){
-	        return dfs(i,j+1,m,n);
-	    }
-	 
-	    return 0;
+
+	public static int dfs(int i, int j, int m, int n) {
+		if (i == m - 1 && j == n - 1) {
+			return 1;
+		}
+
+		if (i < m - 1 && j < n - 1) {
+			return dfs(i + 1, j, m, n) + dfs(i, j + 1, m, n);
+		}
+
+		if (i < m - 1) {
+			return dfs(i + 1, j, m, n);
+		}
+
+		if (j < n - 1) {
+			return dfs(i, j + 1, m, n);
+		}
+
+		return 0;
 	}
-	
+
 	public static int uniquePathsWithDFSWithRestriction(int m, int n) {
-	    return dfsWithRestriction(0,0,m,n);
+		return dfsWithRestriction(0, 0, m, n);
 	}
-	 
-	public static int dfsWithRestriction(int i, int j, int m, int n){
-	    if(i==m-1 && j==n-1&&i<=j){
-	        return 1;
-	    }
-	 
-	    if(i<m-1 && j<n-1&&i<=j){
-	        return dfsWithRestriction(i+1,j,m,n) + dfsWithRestriction(i,j+1,m,n);
-	    }
-	 
-	    if(i<m-1&&i<=j){
-	        return dfsWithRestriction(i+1,j,m,n);
-	    }
-	 
-	    if(j<n-1&&i<=j){
-	        return dfsWithRestriction(i,j+1,m,n);
-	    }
-	 
-	    return 0;
+
+	public static int dfsWithRestriction(int i, int j, int m, int n) {
+		if (i == m - 1 && j == n - 1 && i <= j) {
+			return 1;
+		}
+
+		if (i < m - 1 && j < n - 1 && i <= j) {
+			return dfsWithRestriction(i + 1, j, m, n) + dfsWithRestriction(i, j + 1, m, n);
+		}
+
+		if (i < m - 1 && i <= j) {
+			return dfsWithRestriction(i + 1, j, m, n);
+		}
+
+		if (j < n - 1 && i <= j) {
+			return dfsWithRestriction(i, j + 1, m, n);
+		}
+
+		return 0;
 	}
-	
+
 	public static int uniquePathsWithDynamicProgramming(int m, int n) {
-	    if(m==0 || n==0) return 0;
-	    if(m==1 || n==1) return 1;
-	 
-	    int[][] dp = new int[m][n];
-	 
-	    //left column
-	    for(int i=0; i<m; i++){
-	        dp[i][0] = 1;
-	    }
-	 
-	    //top row
-	    for(int j=0; j<n; j++){
-	        dp[0][j] = 1;
-	    }
-	 
-	    //fill up the dp table
-	    for(int i=1; i<m; i++){
-	        for(int j=1; j<n; j++){
-	            dp[i][j] = dp[i-1][j] + dp[i][j-1];
-	        }
-	    }
-	 
-	    return dp[m-1][n-1];
+		if (m == 0 || n == 0)
+			return 0;
+		if (m == 1 || n == 1)
+			return 1;
+
+		int[][] dp = new int[m][n];
+
+		// left column
+		for (int i = 0; i < m; i++) {
+			dp[i][0] = 1;
+		}
+
+		// top row
+		for (int j = 0; j < n; j++) {
+			dp[0][j] = 1;
+		}
+
+		// fill up the dp table
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+			}
+		}
+
+		return dp[m - 1][n - 1];
 	}
 
 }
