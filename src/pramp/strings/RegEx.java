2b144b0368506d9e9865ad0b3b51bc1586ef17fb
Julia Chen <juliachenjing@gmail.com>
2017-08-26 22:22:35 -0700

    regex

--- src/pramp/strings/RegEx.java
+++ src/pramp/strings/RegEx.java
@@ -1,4 +1,5 @@
 package pramp.strings;
+
 public class RegEx {
 
 	/*
@@ -37,9 +38,53 @@ public class RegEx {
 		System.out.println(isMatch("abc", "ab*"));
 		System.out.println(isMatch("c", "*"));
 		System.out.println(isMatch("abc", "a.c"));
-		System.out.println(isMatch("abc", "*"));
+		System.out.println(isMatch("abc", "*a"));
 		System.out.println(isMatch("abc", ".b*"));
 
+	}
+
+	/*
+	 * 
+	 * 1, If p.charAt(j) == s.charAt(i) :  dp[i][j] = dp[i-1][j-1];
+       2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
+       3, If p.charAt(j) == '*': 
+          here are two sub conditions:
+               1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
+               2   if p.charAt(i-1) == s.charAt(i) or p.charAt(i-1) == '.':
+                              dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a 
+                           or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
+                           or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
+	 */
+	public boolean isMatch_perfect(String s, String p) {
+
+		if (s == null || p == null) {
+			return false;
+		}
+		boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+		dp[0][0] = true;
+		for (int i = 0; i < p.length(); i++) {
+			if (p.charAt(i) == '*' && dp[0][i - 1]) {
+				dp[0][i + 1] = true;
+			}
+		}
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = 0; j < p.length(); j++) {
+				if (p.charAt(j) == '.') {
+					dp[i + 1][j + 1] = dp[i][j];
+				}
+				if (p.charAt(j) == s.charAt(i)) {
+					dp[i + 1][j + 1] = dp[i][j];
+				}
+				if (p.charAt(j) == '*') {
+					if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
+						dp[i + 1][j + 1] = dp[i + 1][j - 1];
+					} else {
+						dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+					}
+				}
+			}
+		}
+		return dp[s.length()][p.length()];
 	}
 
 	static boolean isMatch(String text, String pattern) {
@@ -50,7 +95,7 @@ public class RegEx {
 
 	static boolean isMatchHelper(String text, String pattern, int textIndex, int patIndex) {
 		// base cases - one of the indexes reached the end of text or pattern
-		if (textIndex >= text.length()){
+		if (textIndex >= text.length()) {
 			if (patIndex >= pattern.length())
 				return true;
 			else {
@@ -62,7 +107,7 @@ public class RegEx {
 		}
 
 		else if ((patIndex >= pattern.length()) && (textIndex < text.length()))
-	        return false;
+			return false;
 
 		// string matching for '.'
 		else if (pattern.charAt(patIndex) == '.') {
@@ -76,8 +121,7 @@ public class RegEx {
 
 		// string matching for letter
 
-		else if (pattern.charAt(patIndex) == text.charAt(textIndex))
-		{
+		else if (pattern.charAt(patIndex) == text.charAt(textIndex)) {
 
 			textIndex++;
 			patIndex++;
@@ -92,7 +136,7 @@ public class RegEx {
 						|| isMatchHelper(text, pattern, textIndex + 1, patIndex));
 			else
 				return isMatchHelper(text, pattern, textIndex, patIndex + 2);
-		else 
+		else
 			return false;
 	}
 
