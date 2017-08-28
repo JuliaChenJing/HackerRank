16094c000f945cae97d2bbc8aee20056eab71f9b
Julia Chen <juliachenjing@gmail.com>
2017-08-27 20:39:33 -0700

    mergeList

--- src/datastructure/linkedlist/MergeTwoSortedLinkedList.java
+++ src/datastructure/linkedlist/MergeTwoSortedLinkedList.java
@@ -1,5 +1,7 @@
 package datastructure.linkedlist;
 
+import java.util.ArrayList;
+
 //https://www.hackerrank.com/challenges/merge-two-sorted-linked-lists/problem
 public class MergeTwoSortedLinkedList {
 
@@ -8,18 +10,17 @@ public class MergeTwoSortedLinkedList {
 	 * list Node is defined as class Node { int data; Node next; }
 	 */
 
-	
 	public static void main(String[] args) {
 		Node a = new Node(1);
 		Node b = new Node(2);
 		Node c = new Node(5);
-		
+
 		Node d = new Node(0);
 		Node e = new Node(3);
 		Node f = new Node(8);
 		a.next = b;
 		b.next = c;
-	
+
 		d.next = e;
 		e.next = f;
 		Node n = a;
@@ -29,14 +30,20 @@ public class MergeTwoSortedLinkedList {
 			n = n.next;
 		}
 		System.out.println();
-	    n = d;
+		n = d;
 		while (n != null) {
 			System.out.print(n.data + "--->");
 			n = n.next;
 		}
 
-		n=mergeLists(a,d);
+		// n = mergeLists(a, d);
+		// n = merge(a, d);
 
+		Node[] list = new Node[2];
+		list[0] = a;
+		list[1] = d;
+		n = mergeKLists(list);
+		
 		System.out.println("\nAFTER: ");
 		while (n != null) {
 			System.out.print(n.data + "--->");
@@ -44,6 +51,22 @@ public class MergeTwoSortedLinkedList {
 		}
 
 	}
+
+	// good
+	public static Node merge(Node l1, Node l2) {
+		if (l1 == null)
+			return l2;
+		if (l2 == null)
+			return l1;
+		if (l1.data < l2.data) {
+			l1.next = merge(l1.next, l2);
+			return l1;
+		} else {
+			l2.next = merge(l1, l2.next);
+			return l2;
+		}
+	}
+
 	static Node mergeLists(Node currA, Node currB) {
 
 		if (currA == null) {
@@ -84,4 +107,21 @@ public class MergeTwoSortedLinkedList {
 
 		return head;
 	}
+
+	public static Node mergeKLists(Node[] lists) {
+		return partion(lists, 0, lists.length - 1);
+	}
+
+	public static Node partion(Node[] lists, int s, int e) {
+		if (s == e)
+			return lists[s];
+		if (s < e) {
+			int q = (s + e) / 2;
+			Node l1 = partion(lists, s, q);
+			Node l2 = partion(lists, q + 1, e);
+			return merge(l1, l2);
+		} else
+			return null;
+	}
+
 }
--- src/datastructure/stack/Braces.java
+++ src/datastructure/stack/Braces.java
@@ -65,17 +65,17 @@ public class Braces {
 		return list;
 	}
 
-	public static void backtrack(List<String> list, String str, int open, int close, int max) {
+	public static void backtrack(List<String> list, String str, int left, int right, int max) {
 
 		if (str.length() == max * 2) {
 			list.add(str);
 			return;
 		}
 
-		if (open < max)
-			backtrack(list, str + "(", open + 1, close, max);
-		if (close < open)
-			backtrack(list, str + ")", open, close + 1, max);
+		if (left < max)
+			backtrack(list, str + "(", left + 1, right, max);
+		if (right < left)
+			backtrack(list, str + ")", left, right + 1, max);
 	}
 
 }
