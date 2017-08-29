8c846db64107f3f37d736ed598585314fa177894
Julia Chen <juliachenjing@gmail.com>
2017-08-28 17:33:07 -0700

    fib

---
+++ src/pramp/dynamic_programming/Fib.java
@@ -0,0 +1,48 @@
+package pramp.dynamic_programming;
+
+//https://blog.pramp.com/how-to-solve-any-dynamic-programming-problem-603b6fbbd771
+public class Fib {
+
+	public int fib_1(int n) {
+		if (n == 0)
+			return 0;
+		if (n == 1)
+			return 1;
+		return fib_1(n - 1) + fib_1(n - 2);
+	}
+
+	public int fib_2(int n) {
+		if (n < 2)
+			return n;
+		// Create cache and initialize to -1
+		int[] cache = new int[n + 1];
+		for (int i = 0; i < cache.length; i++)
+			cache[i] = -1;
+		// Fill initial values in cache
+		cache[0] = 0;
+		cache[1] = 1;
+		return fib(n, cache);
+	}
+
+	private int fib(int n, int[] cache) {
+		// If value is set in cache, return it
+		if (cache[n] >= 0)
+			return cache[n];
+		// Otherwise compute result and add it to the cache before returning
+		cache[n] = fib(n - 1, cache) + fib(n - 2, cache);
+		return cache[n];
+	}
+
+	public int fib_3(int n) {
+		if (n == 0)
+			return 0;
+		// Initialize cache
+		int[] cache = new int[n + 1];
+		cache[1] = 1;
+		// Fill cache iteratively
+		for (int i = 2; i <= n; i++) {
+			cache[i] = cache[i - 1] + cache[i - 2];
+		}
+		return cache[n];
+	}
+}
