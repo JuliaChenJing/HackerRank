502d3021aced1f98fc20dd01d58cff581e36e73f
Julia Chen <juliachenjing@gmail.com>
2017-08-29 16:17:06 -0700

    test

--- src/MonkeyJumpStone.java
+++ src/MonkeyJumpStone.java
@@ -2,16 +2,19 @@
 public class MonkeyJumpStone {
 
 	public static void main(String[] args) {
+		
 		System.out.println(solution(new int[] { 1, -1, 0, 2, 3, 5 }, 3));// 2
 		System.out.println(solution(new int[] { 3, 2, 1 }, 1));// 3
 		System.out.println(solution(new int[] { 1, 2, 3, 4, -1, -1, -1 }, 3));// -1
 
 		System.out.println(jumpleAble(new boolean[] { false, false, true }, 2));// false
-		System.out.println(jumpleAble(new boolean[] { false,false, true,false, false, false }, 3));// false
 		System.out.println(jumpleAble(new boolean[] { false, false, false }, 2));// false
+		
+		System.out.println();
 		System.out.println(jumpleAble(new boolean[] { true, true, true }, 1));// true
-
 		System.out.println(jumpleAble(new boolean[] { false, false, true }, 3));// true
+		System.out.println(jumpleAble(new boolean[] { false, false, true, false, false, false }, 3));//false
+		
 		System.out.println(jumpleAble(new boolean[] { false, false }, 3));// true
 	}
 
@@ -20,9 +23,8 @@ public class MonkeyJumpStone {
 
 		boolean[] stones = new boolean[A.length];
 
-		for (int i = 0; i < A.length+1; i++) {
-			
-			
+		for (int i = 0; i < A.length + 1; i++) {
+
 			for (int j = 0; j < A.length; j++) {
 				stones[j] = false;
 				if (A[j] == -1)
@@ -30,11 +32,11 @@ public class MonkeyJumpStone {
 				else if (A[j] <= i)
 					stones[j] = true;
 			}
-			if (jumpleAble(stones, D)){
-				
+			if (jumpleAble(stones, D)) {
+
 				return i;
 			}
-				
+
 		}
 		return -1;
 	}
@@ -53,15 +55,17 @@ public class MonkeyJumpStone {
 			}
 
 			right++;
-
 		}
 
-		right--;
-		left--;
-		System.out.print("---"+left);
-		System.out.println("---"+right);
-		if (!stones[right-1])
-			return stones.length - left > D ? false : true;
+		//System.out.println(left);
+		//System.out.println(right);
+		if (!stones[right - 2]){
+			if(left==-1)
+				return stones.length<D;
+			return stones.length - left+1 < D ;
+			
+		}
+			
 		return true;
 	}
 }
