bcd001ec5add9d89e90a610300d792b11c53d6a2
Julia Chen <juliachenjing@gmail.com>
2017-08-29 15:39:51 -0700

    silly head

--- src/MonkeyJumpStone.java
+++ src/MonkeyJumpStone.java
@@ -5,46 +5,56 @@ public class MonkeyJumpStone {
 		System.out.println(solution(new int[] { 1, -1, 0, 2, 3, 5 }, 3));// 2
 		System.out.println(solution(new int[] { 3, 2, 1 }, 1));// 3
 		System.out.println(solution(new int[] { 1, 2, 3, 4, -1, -1, -1 }, 3));// -1
-		System.out.println(jumpleAble(new boolean[] {false, false,true}, 3));// true
-		System.out.println(jumpleAble(new boolean[] {false, false,true}, 2));// false
+		System.out.println(jumpleAble(new boolean[] { false, false, true }, 3));// true
+		System.out.println(jumpleAble(new boolean[] { false, false, true }, 2));// false
+		System.out.println(jumpleAble(new boolean[] { false, false }, 3));// true
+		System.out.println(jumpleAble(new boolean[] { false, false, false }, 2));// false
+		System.out.println(jumpleAble(new boolean[] { true, true, true }, 1));// true
 	}
 
 	public static int solution(int[] A, int D) {
 		// write your code in Java SE 8
+
 		boolean[] stones = new boolean[A.length];
 
-		int re = 0;
 		for (int i = 0; i < A.length; i++) {
-			for (boolean b : stones)
-				b = false;
-			for (int j = 0; A.length < j; j++) {
+			stones[i] = false;
+			for (int j = 0; j < A.length; j++) {
 				if (A[j] == -1)
 					continue;
-				if (A[j] > i)
+				if (A[j] <= i)
 					stones[i] = true;
 			}
-			if (jumpleAble(stones, D))
-				re++;
+			if (jumpleAble(stones, D)){
+				
+				return i;
+			}
+				
 		}
-		return re;
+		return -1;
 	}
 
 	private static boolean jumpleAble(boolean[] stones, int D) {
 		int left = -1;
-		int right = 1;
+		int right = 0;
+		// for (boolean b : stones)
+		// System.out.print(b+" : ");
 		while (right < stones.length) {
 			if (stones[right]) {
 				if (right - left > D)
 					return false;
-				else{
+				else {
 					right++;
 					left = right;
 				}
 			}
-			
+
 			right++;
-			
+
 		}
+
+		if (!stones[right - 2])
+			return stones.length - left > D ? false : true;
 		return true;
 	}
 }
