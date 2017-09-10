ad0afb10d379e4a572aab2566ee9fa157e52a631
Julia Chen <juliachenjing@gmail.com>
2017-09-09 20:51:52 -0700

    solve

--- src/pramp/life_question/ValidSudoku.java
+++ src/pramp/life_question/ValidSudoku.java
@@ -2,6 +2,7 @@ package pramp.life_question;
 
 import java.util.HashSet;
 
+//https://leetcode.com/problems/valid-sudoku/description/
 public class ValidSudoku {
 	/*
 	 * Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
@@ -31,6 +32,55 @@ public class ValidSudoku {
 						&& !cube.add(board[RowIndex + j / 3][ColIndex + j % 3]))
 					return false;
 			}
+		}
+		return true;
+	}
+
+	/*
+	 * Try 1 through 9 for each cell. The time complexity should be 9 ^ m (m
+	 * represents the number of blanks to be filled in), since each blank can
+	 * have 9 choices. Details see comments inside code. Let me know your
+	 * suggestions.
+	 * 
+	 * Sorry for being late to answer the time complexity question
+	 */
+	public void solveSudoku(char[][] board) {
+		if (board == null || board.length == 0)
+			return;
+		solve(board);
+	}
+
+	public boolean solve(char[][] board) {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				if (board[i][j] == '.') {
+					for (char c = '1'; c <= '9'; c++) {// trial. Try 1 through 9
+						if (isValid(board, i, j, c)) {
+							board[i][j] = c; // Put c for this cell
+
+							if (solve(board))
+								return true; // If it's the solution return true
+							else
+								board[i][j] = '.'; // Otherwise go back
+						}
+					}
+
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private boolean isValid(char[][] board, int row, int col, char c) {
+		for (int i = 0; i < 9; i++) {
+			if (board[i][col] != '.' && board[i][col] == c)
+				return false; // check row
+			if (board[row][i] != '.' && board[row][i] == c)
+				return false; // check column
+			if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] != '.'
+					&& board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+				return false; // check 3*3 block
 		}
 		return true;
 	}
