f013d922f29e72f4fa9c6d18565c24d2129fa3d2
Julia Chen <juliachenjing@gmail.com>
2017-09-10 23:19:32 -0700

    sum

---
+++ src/datastructure/array/sortedArrays/Sum_Combination.java
@@ -0,0 +1,24 @@
+package datastructure.array.sortedArrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sum_Combination {
+
+	/*
+	 * Given a set of candidate numbers (C) (without duplicates) and a target
+	 * number (T), find all unique combinations in C where the candidate numbers
+	 * sums to T.
+	 * 
+	 * The same repeated number may be chosen from C unlimited number of times.
+	 * 
+	 * Note: All numbers (including target) will be positive integers. The
+	 * solution set must not contain duplicate combinations. For example, given
+	 * candidate set [2, 3, 6, 7] and target 7, A solution set is: [ [7], [2, 2,
+	 * 3] ]
+	 */
+	public List< List <Integer>> combinationSum(int[] candidates, int target) {
+		List< List <Integer>> re=new ArrayList< List <Integer>>();
+		return re;
+	}
+}
--- src/pramp/life_question/ValidSudoku.java
+++ src/pramp/life_question/ValidSudoku.java
@@ -53,8 +53,11 @@ public class ValidSudoku {
 	public boolean solve(char[][] board) {
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[0].length; j++) {
+				
 				if (board[i][j] == '.') {
+					
 					for (char c = '1'; c <= '9'; c++) {// trial. Try 1 through 9
+						
 						if (isValid(board, i, j, c)) {
 							board[i][j] = c; // Put c for this cell
 
@@ -74,10 +77,13 @@ public class ValidSudoku {
 
 	private boolean isValid(char[][] board, int row, int col, char c) {
 		for (int i = 0; i < 9; i++) {
+			
 			if (board[i][col] != '.' && board[i][col] == c)
 				return false; // check row
+			
 			if (board[row][i] != '.' && board[row][i] == c)
 				return false; // check column
+			
 			if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] != '.'
 					&& board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
 				return false; // check 3*3 block
