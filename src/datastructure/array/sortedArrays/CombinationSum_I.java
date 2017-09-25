d2b3563a8f9e3b48c3b117082e0af72284da542f
Julia Chen <juliachenjing@gmail.com>
2017-09-24 18:26:13 -0700

    sum

--- src/datastructure/array/sortedArrays/CombinationSum_I.java
+++ src/datastructure/array/sortedArrays/CombinationSum_I.java
@@ -20,24 +20,32 @@ A solution set is:
   [2, 2, 3]
 ]
  */
-public class CombinationSum {
+public class CombinationSum_I {
 
 	public List<List<Integer>> combinationSum(int[] nums, int target) {
-	    List<List<Integer>> list = new ArrayList<>();
+		//initiate the array
+	    List<List<Integer>> re = new ArrayList<>();
+	    //sort the array
 	    Arrays.sort(nums);
-	    backtrack(list, new ArrayList<>(), nums, target, 0);
-	    return list;
+        //call helper method
+	    backtrack(re, new ArrayList<>(), nums, target, 0);
+	    return re;
 	}
 
 	private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-	    if(remain < 0) return;
-	    else if(remain == 0) list.add(new ArrayList<>(tempList));
+	    if(remain < 0) 
+	    	return;
+	    else
+	    	if(remain == 0) 
+	    		list.add(tempList);
 	    else{ 
-	        for(int i = start; i < nums.length; i++){
+	    	for(int i = start; i < nums.length; i++){
 	            tempList.add(nums[i]);
 	            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
 	            tempList.remove(tempList.size() - 1);
 	        }
 	    }
 	}
+	
+
 }
