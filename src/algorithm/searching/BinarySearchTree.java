ab4ab78d0492711c72962223e34ef4fcdbb44f04
Julia Chen <juliachenjing@gmail.com>
2017-11-17 17:03:00 +0800

    checked

--- src/algorithm/searching/BinarySearchTree.java
+++ src/algorithm/searching/BinarySearchTree.java
@@ -1,6 +1,5 @@
 package algorithm.searching;
 
-
 //BinarySearchTree class
 //
 //CONSTRUCTION: with no initializer
@@ -18,278 +17,277 @@ package algorithm.searching;
 //Throws UnderflowException as appropriate
 
 /**
-* Implements an unbalanced binary search tree.
-* Note that all "matching" is based on the compareTo method.
-* @author Mark Allen Weiss
-*/
-public class BinarySearchTree<AnyType extends Comparable<? super AnyType>>
-{
- /**
-  * Construct the tree.
-  */
- public BinarySearchTree( )
- {
-     root = null;
- }
- 
- /**
-  * Internal method to find an item in a subtree.
-  * @param x is item to search for.
-  * @param t the node that roots the subtree.
-  * @return node containing the matched item.
-  */
- private boolean contains( AnyType x, BinaryNode<AnyType> t )
- {
-     if( t == null )
-         return false;
-         
-     int compareResult = x.compareTo( t.element );
-         
-     if( compareResult < 0 )
-         return contains( x, t.left );
-     else if( compareResult > 0 )
-         return contains( x, t.right );
-     else
-         return true;    // Match
- }
+ * Implements an unbalanced binary search tree. Note that all "matching" is
+ * based on the compareTo method.
+ * 
+ * @author Mark Allen Weiss
+ */
+public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
+	/**
+	 * Construct the tree.
+	 */
+	public BinarySearchTree() {
+		root = null;
+	}
 
+	/**
+	 * Internal method to find an item in a subtree.
+	 * 
+	 * @param x
+	 *            is item to search for.
+	 * @param t
+	 *            the node that roots the subtree.
+	 * @return node containing the matched item.
+	 */
+	private boolean contains(AnyType x, BinaryNode<AnyType> t) {
+		if (t == null)
+			return false;
 
- /**
-  * Insert into the tree; duplicates are ignored.
-  * @param x the item to insert.
-  */
- public void insert( AnyType x )
- {
-     root = insert( x, root );
- }
+		int compareResult = x.compareTo(t.element);
 
- /**
-  * Remove from the tree. Nothing is done if x is not found.
-  * @param x the item to remove.
-  */
- public void remove( AnyType x )
- {
-     root = remove( x, root );
- }
+		if (compareResult < 0)
+			return contains(x, t.left);
+		else if (compareResult > 0)
+			return contains(x, t.right);
+		else
+			return true; // Match
+	}
 
- /**
-  * Find the smallest item in the tree.
-  * @return smallest item or null if empty.
-  */
- public AnyType findMin( )
- {
-     if( isEmpty( ) )
-        return null;// throw new UnderflowException( );
-     return findMin( root ).element;
- }
+	/**
+	 * Insert into the tree; duplicates are ignored.
+	 * 
+	 * @param x
+	 *            the item to insert.
+	 */
+	public void insert(AnyType x) {
+		root = insert(x, root);
+	}
 
- /**
-  * Find the largest item in the tree.
-  * @return the largest item of null if empty.
-  */
- public AnyType findMax( )
- {
-     if( isEmpty( ) )
-         return null;//throw new UnderflowException( );
-     return findMax( root ).element;
- }
+	/**
+	 * Remove from the tree. Nothing is done if x is not found.
+	 * 
+	 * @param x
+	 *            the item to remove.
+	 */
+	public void remove(AnyType x) {
+		root = remove(x, root);
+	}
 
- /**
-  * Find an item in the tree.
-  * @param x the item to search for.
-  * @return true if not found.
-  */
- public boolean contains( AnyType x )
- {
-     return contains( x, root );
- }
+	/**
+	 * Find the smallest item in the tree.
+	 * 
+	 * @return smallest item or null if empty.
+	 */
+	public AnyType findMin() {
+		if (isEmpty())
+			return null;// throw new UnderflowException( );
+		return findMin(root).element;
+	}
 
- /**
-  * Make the tree logically empty.
-  */
- public void makeEmpty( )
- {
-     root = null;
- }
+	/**
+	 * Find the largest item in the tree.
+	 * 
+	 * @return the largest item of null if empty.
+	 */
+	public AnyType findMax() {
+		if (isEmpty())
+			return null;// throw new UnderflowException( );
+		return findMax(root).element;
+	}
 
- /**
-  * Test if the tree is logically empty.
-  * @return true if empty, false otherwise.
-  */
- public boolean isEmpty( )
- {
-     return root == null;
- }
+	/**
+	 * Find an item in the tree.
+	 * 
+	 * @param x
+	 *            the item to search for.
+	 * @return true if not found.
+	 */
+	public boolean contains(AnyType x) {
+		return contains(x, root);
+	}
 
- /**
-  * Print the tree contents in sorted order.
-  */
- public void printTree( )
- {
-     if( isEmpty( ) )
-         System.out.println( "Empty tree" );
-     else
-         printTree( root );
- }
+	/**
+	 * Make the tree logically empty.
+	 */
+	public void makeEmpty() {
+		root = null;
+	}
 
- /**
-  * Internal method to insert into a subtree.
-  * @param x the item to insert.
-  * @param t the node that roots the subtree.
-  * @return the new root of the subtree.
-  */
- private BinaryNode<AnyType> insert( AnyType x, BinaryNode<AnyType> t )
- {
-     if( t == null )
-         return new BinaryNode<AnyType>( x, null, null );
-     
-     int compareResult = x.compareTo( t.element );
-         
-     if( compareResult < 0 )
-         t.left = insert( x, t.left );
-     else if( compareResult > 0 )
-         t.right = insert( x, t.right );
-     else
-         ;  // Duplicate; do nothing
-     return t;
- }
+	/**
+	 * Test if the tree is logically empty.
+	 * 
+	 * @return true if empty, false otherwise.
+	 */
+	public boolean isEmpty() {
+		return root == null;
+	}
 
- /**
-  * Internal method to remove from a subtree.
-  * @param x the item to remove.
-  * @param t the node that roots the subtree.
-  * @return the new root of the subtree.
-  */
- private BinaryNode<AnyType> remove( AnyType x, BinaryNode<AnyType> t )
- {
-     if( t == null )
-         return t;   // Item not found; do nothing
-         
-     int compareResult = x.compareTo( t.element );
-         
-     if( compareResult < 0 )
-         t.left = remove( x, t.left );
-     else if( compareResult > 0 )
-         t.right = remove( x, t.right );
-     else if( t.left != null && t.right != null ) // Two children
-     {
-         t.element = findMin( t.right ).element;
-         t.right = remove( t.element, t.right );
-     }
-     else
-         t = ( t.left != null ) ? t.left : t.right;
-     return t;
- }
+	/**
+	 * Print the tree contents in sorted order.
+	 */
+	public void printTree() {
+		if (isEmpty())
+			System.out.println("Empty tree");
+		else
+			printTree(root);
+	}
 
- /**
-  * Internal method to find the smallest item in a subtree.
-  * @param t the node that roots the subtree.
-  * @return node containing the smallest item.
-  */
- private BinaryNode<AnyType> findMin( BinaryNode<AnyType> t )
- {
-     if( t == null )
-         return null;
-     else if( t.left == null )
-         return t;
-     return findMin( t.left );
- }
+	/**
+	 * Internal method to insert into a subtree.
+	 * 
+	 * @param x
+	 *            the item to insert.
+	 * @param t
+	 *            the node that roots the subtree.
+	 * @return the new root of the subtree.
+	 */
+	private BinaryNode<AnyType> insert(AnyType x, BinaryNode<AnyType> t) {
+		if (t == null)
+			return new BinaryNode<AnyType>(x, null, null);
 
- /**
-  * Internal method to find the largest item in a subtree.
-  * @param t the node that roots the subtree.
-  * @return node containing the largest item.
-  */
- private BinaryNode<AnyType> findMax( BinaryNode<AnyType> t )
- {
-     if( t != null )
-         while( t.right != null )
-             t = t.right;
+		int compareResult = x.compareTo(t.element);
 
-     return t;
- }
+		if (compareResult < 0)
+			t.left = insert(x, t.left);
+		else if (compareResult > 0)
+			t.right = insert(x, t.right);
+		else
+			; // Duplicate; do nothing
+		return t;
+	}
 
- 
- /**
-  * Internal method to print a subtree in sorted order.
-  * @param t the node that roots the subtree.
-  */
- private void printTree( BinaryNode<AnyType> t )
- {
-     if( t != null )
-     {
-         printTree( t.left );
-         System.out.println( t.element );
-         printTree( t.right );
-     }
- }
+	/**
+	 * Internal method to remove from a subtree.
+	 * 
+	 * @param x
+	 *            the item to remove.
+	 * @param t
+	 *            the node that roots the subtree.
+	 * @return the new root of the subtree.
+	 */
+	private BinaryNode<AnyType> remove(AnyType x, BinaryNode<AnyType> t) {
+		if (t == null)
+			return t; // Item not found; do nothing
 
- /**
-  * Internal method to compute height of a subtree.
-  * @param t the node that roots the subtree.
-  */
- private int height( BinaryNode<AnyType> t )
- {
-     if( t == null )
-         return -1;
-     else
-         return 1 + Math.max( height( t.left ), height( t.right ) );    
- }
- 
- // Basic node stored in unbalanced binary search trees
- private static class BinaryNode<AnyType>
- {
-         // Constructors
-     BinaryNode( AnyType theElement )
-     {
-         this( theElement, null, null );
-     }
+		int compareResult = x.compareTo(t.element);
 
-     BinaryNode( AnyType theElement, BinaryNode<AnyType> lt, BinaryNode<AnyType> rt )
-     {
-         element  = theElement;
-         left     = lt;
-         right    = rt;
-     }
+		if (compareResult < 0)
+			t.left = remove(x, t.left);
+		else if (compareResult > 0)
+			t.right = remove(x, t.right);
+		else if (t.left != null && t.right != null) // Two children
+		{
+			t.element = findMin(t.right).element;
+			t.right = remove(t.element, t.right);
+		} else
+			t = (t.left != null) ? t.left : t.right;
+		return t;
+	}
 
-     AnyType element;            // The data in the node
-     BinaryNode<AnyType> left;   // Left child
-     BinaryNode<AnyType> right;  // Right child
- }
+	/**
+	 * Internal method to find the smallest item in a subtree.
+	 * 
+	 * @param t
+	 *            the node that roots the subtree.
+	 * @return node containing the smallest item.
+	 */
+	private BinaryNode<AnyType> findMin(BinaryNode<AnyType> t) {
+		if (t == null)
+			return null;
+		else if (t.left == null)
+			return t;
+		return findMin(t.left);
+	}
 
+	/**
+	 * Internal method to find the largest item in a subtree.
+	 * 
+	 * @param t
+	 *            the node that roots the subtree.
+	 * @return node containing the largest item.
+	 */
+	private BinaryNode<AnyType> findMax(BinaryNode<AnyType> t) {
+		if (t != null)
+			while (t.right != null)
+				t = t.right;
 
-   /** The tree root. */
- private BinaryNode<AnyType> root;
+		return t;
+	}
 
+	/**
+	 * Internal method to print a subtree in sorted order.
+	 * 
+	 * @param t
+	 *            the node that roots the subtree.
+	 */
+	private void printTree(BinaryNode<AnyType> t) {
+		if (t != null) {
+			printTree(t.left);
+			System.out.println(t.element);
+			printTree(t.right);
+		}
+	}
 
-     // Test program
- public static void main( String [ ] args )
- {
-     BinarySearchTree<Integer> t = new BinarySearchTree<Integer>( );
-     final int NUMS = 4000;
-     final int GAP  =   37;
+	/**
+	 * Internal method to compute height of a subtree.
+	 * 
+	 * @param t
+	 *            the node that roots the subtree.
+	 */
+	private int height(BinaryNode<AnyType> t) {
+		if (t == null)
+			return -1;
+		else
+			return 1 + Math.max(height(t.left), height(t.right));
+	}
 
-     System.out.println( "Checking... (no more output means success)" );
+	// Basic node stored in unbalanced binary search trees
+	private static class BinaryNode<AnyType> {
+		// Constructors
+		BinaryNode(AnyType theElement) {
+			this(theElement, null, null);
+		}
 
-     for( int i = GAP; i != 0; i = ( i + GAP ) % NUMS )
-         t.insert( i );
+		BinaryNode(AnyType theElement, BinaryNode<AnyType> lt, BinaryNode<AnyType> rt) {
+			element = theElement;
+			left = lt;
+			right = rt;
+		}
 
-     for( int i = 1; i < NUMS; i+= 2 )
-         t.remove( i );
+		AnyType element; // The data in the node
+		BinaryNode<AnyType> left; // Left child
+		BinaryNode<AnyType> right; // Right child
+	}
 
-     if( NUMS < 40 )
-         t.printTree( );
-     if( t.findMin( ) != 2 || t.findMax( ) != NUMS - 2 )
-         System.out.println( "FindMin or FindMax error!" );
+	/** The tree root. */
+	private BinaryNode<AnyType> root;
 
-     for( int i = 2; i < NUMS; i+=2 )
-          if( !t.contains( i ) )
-              System.out.println( "Find error1!" );
+	// Test program
+	public static void main(String[] args) {
+		BinarySearchTree<Integer> t = new BinarySearchTree<Integer>();
+		final int NUMS = 4000;
+		final int GAP = 37;
 
-     for( int i = 1; i < NUMS; i+=2 )
-     {
-         if( t.contains( i ) )
-             System.out.println( "Find error2!" );
-     }
- }
+		System.out.println("Checking... (no more output means success)");
+
+		for (int i = GAP; i != 0; i = (i + GAP) % NUMS)
+			t.insert(i);
+
+		for (int i = 1; i < NUMS; i += 2)
+			t.remove(i);
+
+		if (NUMS < 40)
+			t.printTree();
+		if (t.findMin() != 2 || t.findMax() != NUMS - 2)
+			System.out.println("FindMin or FindMax error!");
+
+		for (int i = 2; i < NUMS; i += 2)
+			if (!t.contains(i))
+				System.out.println("Find error1!");
+
+		for (int i = 1; i < NUMS; i += 2) {
+			if (t.contains(i))
+				System.out.println("Find error2!");
+		}
+	}
 }
