2f15e2457959653990e826ff30ccf83201733277
Julia Chen <juliachenjing@gmail.com>
2017-12-14 16:53:40 -0500

    Update Fib.java

--- src/pramp/dynamic_programming/Fib.java
+++ src/pramp/dynamic_programming/Fib.java
@@ -3,6 +3,7 @@ package pramp.dynamic_programming;
 //https://blog.pramp.com/how-to-solve-any-dynamic-programming-problem-603b6fbbd771
 public class Fib {
 
+	//method 1: recursion , bad
 	public int fib_1(int n) {
 		if (n == 0)
 			return 0;
@@ -11,13 +12,16 @@ public class Fib {
 		return fib_1(n - 1) + fib_1(n - 2);
 	}
 
+	//method 2 :use cache and recursion
 	public int fib_2(int n) {
 		if (n < 2)
 			return n;
+		
 		// Create cache and initialize to -1
 		int[] cache = new int[n + 1];
 		for (int i = 0; i < cache.length; i++)
 			cache[i] = -1;
+		
 		// Fill initial values in cache
 		cache[0] = 0;
 		cache[1] = 1;
@@ -33,6 +37,13 @@ public class Fib {
 		return cache[n];
 	}
 
+	
+	/*
+	In computing, memoization or memoisation is an optimization technique used primarily to speed up computer programs by 
+	storing the results of expensive function calls and returning the cached result when the same inputs occur again.
+	*/
+	
+	//method 3:best way memoization
 	public int fib_3(int n) {
 		if (n == 0)
 			return 0;
