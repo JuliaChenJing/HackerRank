d6514d53f2dd06d1cee10387bd94c9c46c962c19
Julia Chen <juliachenjing@gmail.com>
2017-12-14 16:44:08 -0500

    Update SkipBlank.java

--- src/datastructure/string/SkipBlank.java
+++ src/datastructure/string/SkipBlank.java
@@ -3,6 +3,15 @@ package datastructure.string;
 public class SkipBlank {
 	public static void main(String[] args) {
 		String str = "aa bb    cc  dd  ee   cs ";
+
+		System.out.println(skipBlank(str));
+		String s2 = str.replace(" ", "");
+
+		System.out.println(s2);
+
+	}
+
+	public static String skipBlank(String str) {
 		String s = "";
 		for (int i = 0; i < str.length() - 1; i++) {
 			if ((int) str.charAt(i) == 32) {
@@ -12,10 +21,7 @@ public class SkipBlank {
 		}
 		if ((int) str.charAt(str.length() - 1) != 32)
 			s += str.charAt(str.length() - 1);
-		
-		String s2=str.replace(" ", "");
-		System.out.println(s);
-		System.out.println(s2);
-		
+
+		return s;
 	}
 }
