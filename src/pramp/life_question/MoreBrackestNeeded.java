93f3e64b76f11cc2f0b5aa53b015a78af4708299
Julia Chen <juliachenjing@gmail.com>
2018-09-27 11:16:28 -0400

    more brackets needed

--- src/pramp/life_question/MoreBrackestNeeded.java
+++ src/pramp/life_question/MoreBrackestNeeded.java
@@ -1,6 +1,6 @@
 package pramp.life_question;
 
-public class Bracket {
+public class MoreBrackestNeeded {
 
 	/*
 	 * A string of brackets is considered correctly matched if every opening
@@ -9,42 +9,33 @@ public class Bracket {
 	 * “((” aren’t. For instance, “((” could become correctly matched by adding
 	 * two closing brackets at the end, so you’d return 2.
 	 * 
-	 * Given a string that consists of brackets, write a function bracketMatch
-	 * that takes a bracket string as an input and returns the minimum number of
-	 * brackets you’d need to add to the input in order to make it correctly
-	 * matched.
+	 * Given a string that consists of brackets, write a function
+	 * moreBracketsNeeded that takes a bracket string as an input and returns
+	 * the minimum number of brackets you’d need to add to the input in order to
+	 * make it correctly matched.
 	 */
-	static int bracketMatch(String text) {
-		// your code goes here
+	static int moreBracketsNeeded(String text) {
 		int result = 0;
 		if (text == null || text.length() == 0)
 			return 0;
 		int numOfLeft = 0;
 		for (int i = 0; i < text.length(); i++) {
-
 			if (text.charAt(i) == '(')
 				numOfLeft++;
 			else { // ')'
-
 				if (numOfLeft > 0)
 					numOfLeft--;
 				else
 					result++;
-
 			}
-
 		}
-
 		result += numOfLeft;
 		return result;
-
 	}
 
 	public static void main(String[] args) {
-
-		System.out.println(bracketMatch("())("));// 2
-		System.out.println(bracketMatch("())"));// 1
-		System.out.println(bracketMatch("()"));// 0
+		System.out.println(moreBracketsNeeded("())("));// 2
+		System.out.println(moreBracketsNeeded("())"));// 1
+		System.out.println(moreBracketsNeeded("()"));// 0
 	}
-	
 }
