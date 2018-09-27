9b9a423953b451bed4ec0458f7c4af3cebd04f59
Julia Chen <juliachenjing@gmail.com>
2018-09-27 16:59:22 -0400

    Is leap year

---
+++ src/pramp/numbers/IsLeapYear.java
@@ -0,0 +1,27 @@
+package pramp.numbers;
+
+import java.util.Scanner;
+
+/**
+ * Class: Fundamental Programming Practice <br />
+ * 5 @author "Jing Chen" <br />
+ * 6 Description: Check the given year is Leap year or not using ternary
+ * operator. <br />
+ * 7
+ */
+
+public class IsLeapYear {
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Input a year: ");
+		int year = keyboard.nextInt();
+		if (year < 0)
+			System.out.printf("Are you sure this is a year?");
+		boolean isLeapYear = (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) ? true : false;
+		if (isLeapYear == true)
+			System.out.printf("\n The year %d is a leap year", year);
+		else
+			System.out.printf("\n The year %d is not a leap year", year);
+	}
+}
