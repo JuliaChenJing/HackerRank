ab3fe2c856b9951dc15bd00ef3772bfb2d3170d6
Julia Chen <juliachenjing@gmail.com>
2018-09-27 10:13:53 -0400

    BinarySearchTree

--- src/pramp/tree/BinarySearchTree.java
+++ src/pramp/tree/BinarySearchTree.java
@@ -1,10 +1,11 @@
 package pramp.tree;
-public class BinarySearchTree {
 
+public class BinarySearchTree {
 	Node root;
+
 	public static void main(String[] args) {
 
-		Node testNode = null, succ = null;
+		Node testNode = null, succNode = null;
 
 		// Create a Binary Search Tree
 		BinarySearchTree tree = new BinarySearchTree();
@@ -16,62 +17,51 @@ public class BinarySearchTree {
 		tree.insert(11);
 		tree.insert(14);
 
-		// Get a reference to the node whose key is 9
+		// Get a reference to the node whose key is 14
 		testNode = tree.getNodeByKey(14);
 
-		// Find the in order successor of test
-		succ = tree.findInOrderSuccessor(testNode);
+		// Find the in order successor of testNode
+		succNode = tree.findInOrderSuccessor(testNode);
 
 		// Print the key of the successor node
-		if (succ != null) {
-			System.out.println("Inorder successor of " + testNode.key + " is " + succ.key);
+		if (succNode != null) {
+			System.out.println("Inorder successor of node " + testNode.key + " is node " + succNode.key);
 		} else {
-			System.out.println("Inorder successor does not exist");
+			System.out.println("Inorder successor node does not exist");
 		}
 	}
 
-	
-
+	//https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
 	Node findInOrderSuccessor(Node inputNode) {
-
 		if (inputNode.right != null) {// right node is not null
-
 			Node node = inputNode.right;
 			while (node.left != null)
 				node = node.left;
 			return node;
-		}
-		
-		else {// right node is null
+		} else {// right node is null
 			Node parent = inputNode.parent;
 			Node node = inputNode;
-
 			while (parent.left != node) {
 				node = parent;
 				parent = parent.parent;
 			}
-
 			return parent;
-
 		}
 	}
 
 	// Given a binary search tree and a number, inserts a new node
 	// with the given number in the correct place in the tree
 	void insert(int key) {
-
 		// 1. If the tree is empty, create the root
 		if (root == null) {
 			root = new Node(key);
 			return;
 		}
-
 		// 2) Otherwise, create a node with the key
 		// and traverse down the tree to find where to
 		// to insert the new node
 		Node currentNode = root;
 		Node newNode = new Node(key);
-
 		while (currentNode != null) {
 			if (key < currentNode.key) {
 				if (currentNode.left == null) {
@@ -98,25 +88,20 @@ public class BinarySearchTree {
 	// findInOrderSuccessor method on
 	Node getNodeByKey(int key) {
 		Node currentNode = root;
-
 		while (currentNode != null) {
 			if (key == currentNode.key) {
 				return currentNode;
 			}
-
 			if (key < currentNode.key) {
 				currentNode = currentNode.left;
 			} else {
 				currentNode = currentNode.right;
 			}
 		}
-
 		return null;
 	}
 
-	
 	class Node {
-
 		int key;
 		Node left;
 		Node right;
@@ -130,5 +115,3 @@ public class BinarySearchTree {
 		}
 	}
 }
-
-
