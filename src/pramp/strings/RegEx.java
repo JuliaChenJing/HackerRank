ae34ef8f19171446026e89fede60b32d1de99833
Julia Chen <juliachenjing@gmail.com>
2018-09-27 10:37:07 -0400

    Reverse Sentence

--- src/pramp/strings/RegEx.java
+++ src/pramp/strings/RegEx.java
@@ -29,34 +29,31 @@ public class RegEx {
 	 * 
 	 * input: text = "abbb", pattern = "ab*" output: true
 	 * 
-	 * input: text = "acd", pattern = "ab*c." output: true
+	 * input: text = "abc", pattern = "ab*c." output: true
 	 */
 
 	public static void main(String[] args) {
-
 		System.out.println(isMatch("", "*"));
 		System.out.println(isMatch("abc", "ab*"));
 		System.out.println(isMatch("c", "*"));
 		System.out.println(isMatch("abc", "a.c"));
-		System.out.println(isMatch("abc", "*a"));
+		System.out.println(isMatch("abc", "*a*"));
 		System.out.println(isMatch("abc", ".b*"));
 
 	}
 
 	/*
 	 * 
-	 * 1, If p.charAt(j) == s.charAt(i) :  dp[i][j] = dp[i-1][j-1];
-       2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
-       3, If p.charAt(j) == '*': 
-          here are two sub conditions:
-               1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
-               2   if p.charAt(i-1) == s.charAt(i) or p.charAt(i-1) == '.':
-                              dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a 
-                           or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
-                           or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
+	 * 1, If p.charAt(j) == s.charAt(i) : dp[i][j] = dp[i-1][j-1]; 2, If
+	 * p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1]; 3, If p.charAt(j) == '*':
+	 * here are two sub conditions: 1 if p.charAt(j-1) != s.charAt(i) : dp[i][j]
+	 * = dp[i][j-2] //in this case, a* only counts as empty 2 if p.charAt(i-1)
+	 * == s.charAt(i) or p.charAt(i-1) == '.': dp[i][j] = dp[i-1][j] //in this
+	 * case, a* counts as multiple a or dp[i][j] = dp[i][j-1] // in this case,
+	 * a* counts as single a or dp[i][j] = dp[i][j-2] // in this case, a* counts
+	 * as empty
 	 */
 	public boolean isMatch_perfect(String s, String p) {
-
 		if (s == null || p == null) {
 			return false;
 		}
@@ -88,9 +85,7 @@ public class RegEx {
 	}
 
 	static boolean isMatch(String text, String pattern) {
-
 		return isMatchHelper(text, pattern, 0, 0);
-
 	}
 
 	static boolean isMatchHelper(String text, String pattern, int textIndex, int patIndex) {
@@ -104,28 +99,20 @@ public class RegEx {
 				else
 					return false;
 			}
-		}
-
-		else if ((patIndex >= pattern.length()) && (textIndex < text.length()))
+		} else if ((patIndex >= pattern.length()) && (textIndex < text.length()))
 			return false;
 
 		// string matching for '.'
 		else if (pattern.charAt(patIndex) == '.') {
-
 			textIndex++;
 			patIndex++;
-
 			return isMatchHelper(text, pattern, textIndex, patIndex);
-
 		}
 
 		// string matching for letter
-
 		else if (pattern.charAt(patIndex) == text.charAt(textIndex)) {
-
 			textIndex++;
 			patIndex++;
-
 			return isMatchHelper(text, pattern, textIndex, patIndex);
 		}
 
