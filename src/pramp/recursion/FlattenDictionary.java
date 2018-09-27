1210d2a56cda34626984d3259dff12ffc89755aa
Julia Chen <juliachenjing@gmail.com>
2018-09-27 10:50:53 -0400

    formatted

--- src/pramp/recursion/FlattenDictionary.java
+++ src/pramp/recursion/FlattenDictionary.java
@@ -1,10 +1,8 @@
 package pramp.recursion;
 
-
 import java.util.HashMap;
 
 public class FlattenDictionary {
-
 	/*
 	 * 
 input:  dict = {
@@ -27,29 +25,23 @@ output: {
             "Key2.c.e" : "1"
         }
 	 */
-	//https://github.com/JuliaChenJing/Web-Application-Collection/blob/master/javaScriptPractice/flatten_dictionary.js
-	public static void main(String [] args)
-	{
-		HashMap<String, Object> dict=new HashMap <String, Object>();
+	// https://github.com/JuliaChenJing/Web-Application-Collection/blob/master/javaScriptPractice/flatten_dictionary.js
+	public static void main(String[] args) {
+		HashMap<String, Object> dict = new HashMap<String, Object>();
 		dict.put("Key1", "1");
-		HashMap <String, Object> key2=new HashMap  <String, Object>();
-		key2.put("a","2");
-		key2.put("b","3");
+		HashMap<String, Object> key2 = new HashMap<String, Object>();
+		key2.put("a", "2");
+		key2.put("b", "3");
 		dict.put("Key2", key2);
-		
+
 		System.out.println(dict);
 		System.out.println(flattenDictionary(dict));
-		
 	}
 
 	static HashMap<String, String> flattenDictionary(HashMap<String, Object> dict) {
-		
 		HashMap<String, String> flatDictionary = new HashMap<String, String>();
-
 		flattenDictionaryHelper("", dict, flatDictionary);
-
 		return flatDictionary;
-
 	}
 
 	static HashMap<String, String> flattenDictionaryHelper(String initialKey, HashMap<String, Object> dict,
@@ -59,37 +51,28 @@ output: {
 
 			value = dict.get(key);
 
-			if (!isDictionary(value)) // test if the value is of a String of integer
+			if (!isDictionary(value)) // test if the value is of a String of
+										// integer
 			{
 				if (initialKey == null || initialKey == "")
 					flatDictionary.put(key, (String) value);
 				else
 					flatDictionary.put(initialKey + "." + key, (String) value);
 			} else {
-
 				if (initialKey == null || initialKey == "")
-
-					flattenDictionaryHelper(key, (HashMap <String, Object>) value, flatDictionary);
+					flattenDictionaryHelper(key, (HashMap<String, Object>) value, flatDictionary);
 				else
-
 					flattenDictionaryHelper(initialKey + "." + key, (HashMap<String, Object>) value, flatDictionary);
 			}
 		}
-
 		return flatDictionary;
 	}
 
-
-
 	// test if the value is of a String of integer
 	private static boolean isDictionary(Object value) {
-		
-	   String s=(String)value;
-	   if (s.matches("/d"))
-		return true;
-	   return false;
+		String s = (String) value;
+		if (s.matches("/d"))
+			return true;
+		return false;
 	}
-
-	
-	
 }
