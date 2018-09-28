817dbb6dd3d0690f30484e495dbaae600c668cad
Julia Chen <juliachenjing@gmail.com>
2018-09-28 10:04:28 -0400

    monkey jump stone

--- src/pramp/life_question/MonkeyJumpStone.java
+++ src/pramp/life_question/MonkeyJumpStone.java
@@ -1,32 +1,24 @@
 package pramp.life_question;
+
 public class MonkeyJumpStone {
 
-public static void main(String[] args) {
-		
-		System.out.println(solution(new int[] { 1, -1, 0, 2, 3, 5 }, 3));// 2
-		System.out.println(solution(new int[] { 3, 2, 1 }, 1));// 3
-		System.out.println(solution(new int[] { 1, 2, 3, 4, -1, -1, -1 }, 3));// -1
-
-		System.out.println(jumpleAble(new boolean[] { false, false, true }, 2));// false
-		System.out.println(jumpleAble(new boolean[] { false, false, false }, 2));// false
-		
+	public static void main(String[] args) {
+		System.out.println(jumpableWithArray(new int[] { 1, -1, 0, 2, 3, 5 }, 3));// 2
+		System.out.println(jumpableWithArray(new int[] { 3, 2, 1 }, 1));// 3
+		System.out.println(jumpableWithArray(new int[] { 1, 2, 3, 4, -1, -1, -1 }, 3));// -1
+		System.out.println(jumpableWithBoolean(new boolean[] { false, false, true }, 2));// false
+		System.out.println(jumpableWithBoolean(new boolean[] { false, false, false }, 2));// false
 		System.out.println();
-		System.out.println(jumpleAble(new boolean[] { true, true, true }, 1));// true
-		System.out.println(jumpleAble(new boolean[] { false, false, true }, 3));// true
-		System.out.println(jumpleAble(new boolean[] { false, false, true, false, false, false }, 3));//false
-		
-		System.out.println(jumpleAble(new boolean[] { false, false }, 3));// true
-		
-		System.out.println(jumpleAble(new boolean[] {true,false ,true,true, false, false }, 3));// true
+		System.out.println(jumpableWithBoolean(new boolean[] { true, true, true }, 1));// true
+		System.out.println(jumpableWithBoolean(new boolean[] { false, false, true }, 3));// true
+		System.out.println(jumpableWithBoolean(new boolean[] { false, false, true, false, false, false }, 3));// false
+		System.out.println(jumpableWithBoolean(new boolean[] { false, false }, 3));// true
+		System.out.println(jumpableWithBoolean(new boolean[] { true, false, true, true, false, false }, 3));// true
 	}
 
-	public static int solution(int[] A, int D) {
-		// write your code in Java SE 8
-
+	public static int jumpableWithArray(int[] A, int D) {
 		boolean[] stones = new boolean[A.length];
-
 		for (int i = 0; i < A.length + 1; i++) {
-
 			for (int j = 0; j < A.length; j++) {
 				stones[j] = false;
 				if (A[j] == -1)
@@ -34,16 +26,15 @@ public static void main(String[] args) {
 				else if (A[j] <= i)
 					stones[j] = true;
 			}
-			if (jumpleAble(stones, D)) {
+			if (jumpableWithBoolean(stones, D)) {
 
 				return i;
 			}
-
 		}
 		return -1;
 	}
 
-	private static boolean jumpleAble(boolean[] stones, int D) {
+	private static boolean jumpableWithBoolean(boolean[] stones, int D) {
 		int left = -1;
 		int right = 0;
 		while (right < stones.length) {
@@ -55,19 +46,15 @@ public static void main(String[] args) {
 					left = right;
 				}
 			}
-
 			right++;
 		}
-
-		//System.out.println(left);
-		//System.out.println(right);
-		if (!stones[right -2]){
-			if(left==-1)
-				return stones.length<D;
-			return stones.length - left+1 < D ;
-			
+		// System.out.println(left);
+		// System.out.println(right);
+		if (!stones[right - 2]) {
+			if (left == -1)
+				return stones.length < D;
+			return stones.length - left + 1 < D;
 		}
-			
 		return true;
 	}
 }
