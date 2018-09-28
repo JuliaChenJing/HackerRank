eabda18f3bcdcd833c2b7bd67755afdf1fa2a615
Julia Chen <juliachenjing@gmail.com>
2018-09-28 13:08:38 -0400

    rename

--- src/pramp/life_question/PairOfDice.java
+++ src/pramp/life_question/PairOfDice.java
@@ -6,8 +6,8 @@ Description:  Complete the following code by implementing the body<br />
 import java.util.Random;
 
 class PairOfDice {
-	public int die1; // Number showing on the first die.
-	public int die2; // Number showing on the second die.
+	public int dice1; // Number showing on the first dice.
+	public int dice2; // Number showing on the second dice.
 
 	public PairOfDice() {
 		// Constructor. Rolls the dice, so that they initially
@@ -18,17 +18,17 @@ class PairOfDice {
 	public PairOfDice(int val1, int val2) {
 		// Constructor. Creates a pair of dice that
 		// are initially showing the values val1 and val2.
-		die1 = val1;
-		die2 = val2;
+		dice1 = val1;
+		dice2 = val2;
 
 	}
 
 	public void roll() {
-		// Roll the dice by setting each of the dice (die1, die2) to be
+		// Roll the dice by setting each of the dice (dice1, dice2) to be
 		// a random number between 1 and 6.
 		Random random = new Random();
-		die1 = random.nextInt(7);
-		die2 = random.nextInt(7);
+		dice1 = random.nextInt(7);
+		dice2 = random.nextInt(7);
 	}
 
 	public static void main(String[] args)
@@ -45,9 +45,9 @@ class PairOfDice {
 			num++;
 			pair1.roll();
 			pair2.roll();
-			System.out.println("Fist pair come up with total " + (pair1.die1 + pair1.die2));
-			System.out.println("Second pair come up with total " + (pair2.die1 + pair2.die2));
-			if ((pair1.die1 + pair1.die2) == (pair2.die1 + pair2.die2))
+			System.out.println("Fist pair come up with total " + (pair1.dice1 + pair1.dice2));
+			System.out.println("Second pair come up with total " + (pair2.dice1 + pair2.dice2));
+			if ((pair1.dice1 + pair1.dice2) == (pair2.dice1 + pair2.dice2))
 				break;
 		}
 		// Display how many times needed to get the same total
