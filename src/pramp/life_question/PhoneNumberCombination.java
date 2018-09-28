04f45939a2c9c0466647adbb7147fe49d195e283
Julia Chen <juliachenjing@gmail.com>
2018-09-28 13:16:42 -0400

    phone number combination

--- src/pramp/life_question/PhoneNumberCombination.java
+++ src/pramp/life_question/PhoneNumberCombination.java
@@ -19,23 +19,24 @@ public class PhoneNumberCombination {
 	 * "cd", "ce", "cf"].
 	 * 
 	 */
-	public static List<String> letterCombinations(String digits) {
-		LinkedList<String> ans = new LinkedList<String>();
+	public static List<String> getLetterCombinations(String digits) {
+		LinkedList<String> letterCombinations = new LinkedList<String>();
 		String[] mapping = new String[] { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-		ans.add("");
+		letterCombinations.add("");
 		for (int i = 0; i < digits.length(); i++) {
 			int x = Character.getNumericValue(digits.charAt(i));
-			while (ans.peek().length() == i) {
-				String t = ans.remove();
+			while (letterCombinations.peek().length() == i) {
+				String t = letterCombinations.remove();
 				for (char s : mapping[x].toCharArray())
-					ans.add(t + s);
+					letterCombinations.add(t + s);
 			}
 		}
-		return ans;
+		return letterCombinations;
 	}
-	
-	public static void main(String [] args){
-		System.out.println(letterCombinations("012"));//[1a, 1b, 1c]
-		System.out.println(letterCombinations("34"));//[dg, dh, di, eg, eh, ei, fg, fh, fi]
+
+	public static void main(String[] args) {
+		System.out.println(getLetterCombinations("012"));// [1a, 1b, 1c]
+		System.out.println(getLetterCombinations("34"));// [dg, dh, di, eg, eh,
+														// ei, fg, fh, fi]
 	}
 }
