ab755ff9689ec342940f885f1d652d401f991935
Julia Chen <juliachenjing@gmail.com>
2018-09-29 21:33:47 -0400

    Number Minus Pair

--- src/pramp/binary_search_and_sort/NumberMinusPair.java
+++ src/pramp/binary_search_and_sort/NumberMinusPair.java
@@ -24,11 +24,24 @@ public class NumberMinusPair {
 	 * 
 	 * Examples:
 	 * 
-	 * input: arr = [0, -1, -2, 2, 1], k = 1 output: [[0, -1], [-1, -2], [2, 1],
+	 * input: arr = [0, -1, -2, 2, 1], k = 1 
+	 * output: [[0, -1], [-1, -2], [2, 1],
 	 * [1, 0]]
 	 * 
-	 * input: arr = [1, 7, 5, 3, 32, 17, 12], k = 17 output: []
+	 * input: arr = [1, 7, 5, 3, 32, 17, 12], k = 17 
+	 * output: []
 	 */
+	
+	public static void main(String[] args) {
+		int[] arr = { 0, -1, -2, 2, 1 };
+		int[][] result = findPairsWithGivenDifference(arr, 1);
+		for (int i = 0; i < result.length; i++) {
+			for (int j = 0; j < result[0].length; j++)
+				System.out.print(result[i][j] + " ");
+			System.out.println();
+		}
+	}
+	
 	static int[][] findPairsWithGivenDifference(int[] arr, int k) {
 		List<int[]> list = new ArrayList<int[]>();
 		Arrays.sort(arr);
@@ -45,15 +58,5 @@ public class NumberMinusPair {
 			i++;
 		}
 		return result;
-	}
-
-	public static void main(String[] args) {
-		int[] arr = { 0, -1, -2, 2, 1 };
-		int[][] result = findPairsWithGivenDifference(arr, 1);
-		for (int i = 0; i < result.length; i++) {
-			for (int j = 0; j < result[0].length; j++)
-				System.out.print(result[i][j] + " ");
-			System.out.println();
-		}
 	}
 }
