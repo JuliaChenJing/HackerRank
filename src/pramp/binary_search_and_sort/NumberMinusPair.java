cbe129694a27d173978a9401ed70385280b4fd5c
Julia Chen <juliachenjing@gmail.com>
2018-09-29 21:45:07 -0400

    number minus pairs

--- src/pramp/binary_search_and_sort/NumberMinusPair.java
+++ src/pramp/binary_search_and_sort/NumberMinusPair.java
@@ -3,12 +3,6 @@ package pramp.binary_search_and_sort;
 import java.util.*;
 
 public class NumberMinusPair {
-	// Tim Sort : Merge + Quick
-	// Heap Sort: nlog
-	// Merge: nlogn --> O(n)
-	// https://en.wikipedia.org/wiki/Timsort
-	// Time:O(nlogn) Space:O(n)
-	// O(nlogn), O(1)
 
 	/*
 	 * Given an array arr of distinct integers and a nonnegative integer k,
@@ -24,39 +18,47 @@ public class NumberMinusPair {
 	 * 
 	 * Examples:
 	 * 
-	 * input: arr = [0, -1, -2, 2, 1], k = 1 
+	 * input: A = [0, -1, -2, 2, 1], k = 1 
 	 * output: [[0, -1], [-1, -2], [2, 1],
 	 * [1, 0]]
 	 * 
-	 * input: arr = [1, 7, 5, 3, 32, 17, 12], k = 17 
+	 * input: A = [1, 7, 5, 3, 32, 17, 12], k = 17 
 	 * output: []
 	 */
-	
+
 	public static void main(String[] args) {
+
 		int[] arr = { 0, -1, -2, 2, 1 };
 		int[][] result = findPairsWithGivenDifference(arr, 1);
+
 		for (int i = 0; i < result.length; i++) {
 			for (int j = 0; j < result[0].length; j++)
 				System.out.print(result[i][j] + " ");
 			System.out.println();
 		}
 	}
-	
-	static int[][] findPairsWithGivenDifference(int[] arr, int k) {
-		List<int[]> list = new ArrayList<int[]>();
-		Arrays.sort(arr);
-		for (int i = 0; i < arr.length; i++) {
-			if (Arrays.binarySearch(arr, arr[i] + k) < 0)
+
+	static int[][] findPairsWithGivenDifference(int[] A, int k) {
+
+		List<int[]> listOfArray = new ArrayList<int[]>();
+		Arrays.sort(A);
+
+		for (int i = 0; i < A.length; i++) {
+			if (Arrays.binarySearch(A, A[i] + k) < 0)
 				continue;
 			else
-				list.add(new int[] { arr[i], arr[i] + k });
+				listOfArray.add(new int[] { A[i], A[i] + k });
 		}
-		int[][] result = new int[list.size()][2];
+
+		// transfer List<int[]> list to int[][] result
+		int[][] arrayOfArray = new int[listOfArray.size()][2];
 		int i = 0;
-		for (int[] temp : list) {
-			result[i] = temp;
+
+		for (int[] array : listOfArray) {
+			arrayOfArray[i] = array;
 			i++;
 		}
-		return result;
+
+		return arrayOfArray;
 	}
 }
